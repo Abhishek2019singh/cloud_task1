842f4a48821011966011502f5af397557b10d254
Abhishek2019singh <64477686+Abhishek2019singh@users.noreply.github.com>
2020-06-17 18:36:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+welcome to my webpage
+
+<img src= "https://buckettask1.s3.ap-south-1.amazonaws.com/AKS.jpg" width=400 height=400 />
+<br />
+<pre>
+
+<?php
+print "salute to our heroes......"
+
+?>
+</pre>
